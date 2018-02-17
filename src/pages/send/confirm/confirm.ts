722d963741b83bcf25be563e191ce56a65e34d5b
Chainfailure <chainfailure@protonmail.com>
2018-02-17 16:58:51 +0100

    Viacoin: swapped a lot of occurences of "bitcoin" to "viacoin"

--- src/pages/send/confirm/confirm.ts
+++ src/pages/send/confirm/confirm.ts
@@ -765,7 +765,7 @@ export class ConfirmPage extends WalletTabsChild {
     this.hideSlideButton = true;
     if (this.paymentExpired) {
       this.showErrorInfoSheet(
-        this.translate.instant('This bitcoin payment request has expired.')
+        this.translate.instant('This viacoin payment request has expired.')
       );
       return undefined;
     }
