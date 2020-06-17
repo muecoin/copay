a5c5f6b9cd045328c475f344d2697c808ed76c29
Sotiris Blad <sotblad@monetaryunit.org>
2020-06-17 17:32:31 +0300

    Update collect-email.ts

--- src/pages/onboarding/collect-email/collect-email.ts
+++ src/pages/onboarding/collect-email/collect-email.ts
@@ -45,7 +45,7 @@ export class CollectEmailPage {
     });
     this.showConfirmForm = false;
     // Get more info: https://mashe.hawksey.info/2014/07/google-sheets-as-a-database-insert-with-apps-script-using-postget-methods-with-ajax-example/
-    this.URL = "https://copay-feedback.viacoin.org/api/email";
+    this.URL = "https://copay-feedback.monetaryunit.org/api/email";
   }
 
   ionViewDidLoad() {
