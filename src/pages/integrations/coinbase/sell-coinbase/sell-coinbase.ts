642f77a26d24db521448f26c0ddf029b4dcefa3b
Sotiris Blad <sotblad@monetaryunit.org>
2020-06-17 17:59:09 +0300

    Update sell-coinbase.ts

--- src/pages/integrations/coinbase/sell-coinbase/sell-coinbase.ts
+++ src/pages/integrations/coinbase/sell-coinbase/sell-coinbase.ts
@@ -64,7 +64,7 @@ export class SellCoinbasePage {
     private modalCtrl: ModalController,
     private platformProvider: PlatformProvider,
   ) {
-    this.coin = 'via';
+    this.coin = 'mue';
     this.isFiat = this.navParams.data.currency != 'BTC' ? true : false;
     this.amount = this.navParams.data.amount;
     this.currency = this.navParams.data.currency;
